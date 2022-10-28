dafb6ca2b3044d1fae51f8a60d08d7b81a838f16
cybervoigt <cybervoigt@gmail.com>
2022-10-28 11:10:12 -0300

    primeiro commit...

---
+++ olamundo.py
@@ -0,0 +1,20 @@
+"""
+Programa Ola mundo...
+Autor Ricardo Voigt
+Data 27/10
+Versão 1.0
+"""
+
+#entrada
+
+ola = 'ola'
+mundo = 'mundo'
+
+#processamento
+
+olamundo = ola+' '+mundo
+
+
+#saida
+
+print(olamundo)
