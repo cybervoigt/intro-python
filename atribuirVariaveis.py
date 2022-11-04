3992fd8337e498e187e0972b88176b80ddd03567
cybervoigt <cybervoigt@gmail.com>
2022-11-04 16:37:46 -0300

    atribuir variaveis...

---
+++ atribuirVariaveis.py
@@ -0,0 +1,26 @@
+"""
+Exercício de atribuição de variaveis
+Requisito: Faça um programa que entre os números 2 e 3 na memória do computador.
+Autor Ricardo Voigt (https://github.com/cybervoigt)
+Data 31/10/2022
+Versão 0.0.1
+"""
+
+#entrada
+
+numero01 = 2
+numero02 = 3
+
+
+#processamento
+
+# nao ha nenhum processamento neste caso...
+
+soma = numero01 + numero02
+
+#saída
+
+teste = "\nTipo da variavel numero01 = " + str( type(numero01) )
+print(teste)
+
+print(f"\nestes números a seguir estão guardados na memoria do computador: {numero01} e {numero02} (soma = {soma})\n")
