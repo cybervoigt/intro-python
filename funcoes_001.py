fea761733b5f2de9e51bc46489b69c5f1db46186
cybervoigt <cybervoigt@gmail.com>
2022-11-16 08:44:05 -0300

    exercicio de funções...

--- funcoes_001.py
+++ funcoes_001.py
@@ -2,29 +2,43 @@
 Exercicio com exemplos de funções...
 Autor Ricardo Voigt (https://github.com/cybervoigt)
 Data 10/11/22
-Versao 1.0.0.0
+Versao 1.0.0.1
 """
 
 #entrada
 def entrada():
-    numero_1 = input("Digite um numero qualquer no teclado: ")
-    pass
+    digitado = input("Digite um numero qualquer no teclado: ")
+    numero = converte_strtofloat(digitado)
+    return numero
 
 #processamento
-def processamento():
-    pass
+def dobro(num):
+    return num + num
 
+def converte_strtofloat(texto):
+    return float(texto)
 
 #saida
-def saida():
-    pass
+def saida(num,res):
+    print(f"o dobro do número {num} é {res}")
+
 
 #funcao principal
 
 def main():
-    entrada()
+
+    valor = entrada()
+    #print(f"o numero digitado foi {valor}")
+
+    resultado = dobro(valor)
+
+    saida(valor,resultado)
 
 
 #execucao da funcao principal
 
-main()
+# preparando para a "programação modular"
+# quando um projeto terá vários arquivos PY
+if __name__ == "__main__":
+    main()
+
