b69e85f84f3eb923c64b42beee909a398ea31f7f
cybervoigt <cybervoigt@gmail.com>
2022-11-23 13:01:45 -0300

    comentarios e código que lista itens um por um

--- lista_mercado.py
+++ lista_mercado.py
@@ -13,13 +13,21 @@ lista = []
 
 #processamento
 
+# estrutura de repetição "infinita"
 while True:
     item = input("digite o item da lista de compras: ")
+    # controle para o usuário digitar X e sair do laço de repetição
     if item == "X":
         break
+    # adiciona o item na lista
     lista.append(item)
 
 #saida
 
 print(f"lista de compras: {lista}")
+
+# listando os itens um abaixo do outro
+print("\nlistando um item abaixo do outro:")
+for item in lista:
+    print(item)
 
